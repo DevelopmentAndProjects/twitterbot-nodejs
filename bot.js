7a5bd13d34b824c9800b60ecc5786c6a86daeca7
mailforsachin <mailforsachin@gmail.com>
2019-03-15 06:10:18 +0530

    Update bot.js

--- bot.js
+++ bot.js
@@ -52,7 +52,7 @@ function tweetEvent (tweet) {
     txt = txt.replace(/@selftwitterhandle/g, '')
 
     // Start a reply back to the sender
-    var reply = 'Hi @' + name + ' ' + ', Thanks for the mention :)'
+    var reply = 'Hi @' + name + ' ' + ', Thanks for the mention. :)'
 
     console.log(reply)
     // Post that tweet!
@@ -96,4 +96,4 @@ function tweeted (err, reply) {
 retweetLatest()
 // ...and then every hour after that. Time here is in milliseconds, so
 // 1000 ms = 1 second, 1 sec * 60 = 1 min, 1 min * 60 = 1 hour --> 1000 * 60 * 60
-setInterval(retweetLatest, 1000 * 60 * 12)
+setInterval(retweetLatest, 1000 * 60 * 60)
