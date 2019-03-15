1517f27a3eea671c8d3ff519ee689dc608d2904c
mailforsachin <mailforsachin@gmail.com>
2019-03-15 06:06:42 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,8 @@
 // Put your own Twitter App keys here. See README.md for more detail.
-module.exports = {
-  consumer_key: 'blah',
-  consumer_secret: 'blah',
-  access_token: 'blah',
-  access_token_secret: 'blah'
-}
+const twitterApp = {
+  consumer_key: process.env.consumer_key,
+  consumer_secret: process.env.consumer_secret,
+  access_token: process.env.access_token,
+  access_token_secret: process.env.access_token_secret,
+  timeout_ms: 60 * 1000 // optional HTTP request timeout to apply to all requests.
+};
